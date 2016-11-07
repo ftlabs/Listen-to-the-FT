fd8e59e10d1b7f67587db5f24d3cf0bbd42f6601
Sean Tracey <seanmtracey@gmail.com>
2016-11-07 14:18:56 +0000

    Session validation middleware

--- routes/user/index.js
+++ routes/user/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const membership = require('../../bin/lib/membership');
 const myft = require('../../bin/lib/myft');
+const validateSession = require('../../bin/lib/validate-session');
 const debug = require('debug')('listen-to-the-FT:routes:user')
 
 /* GET home page. */
@@ -19,60 +20,15 @@ router.post('/login', function(req, res, next) {
 		})
 		.catch(err => {
 			res.status(503);
+			res.end();
 		})
 	;
 });
 
-router.get('/topics', function(req, res, next) {
-	var userSession = undefined;
-	var isSecure = false;
-	debug(req.headers)
+router.use(validateSession);
 
-	if(req.headers.secureSessionToken){
-		userSession = req.headers.secureSessionToken;
-		isSecure = true;
-	}
-	else if(req.headers.sessionToken){
-		userSession = req.headers.sessionToken;
-		isSecure = false;
-	}
-	else if(req.cookies['FTSession_s']){
-		userSession = req.cookies['FTSession_s'];
-		isSecure = true;
-	}
-	else if (req.cookies['FTSession']){
-		userSession = req.cookies['FTSession'];
-		isSecure = false;
-	}
+router.get('/topics', function(req, res) {
 
-	if(userSession === undefined){
-		throw "No user session can be found"
-	}
-
-	membership.validateSession(userSession, isSecure)
-	.then(response => {
-		debug(response)
-	})
-
-
-
-
-	myft.topics(req.params.userid)
-		.then(result => {
-			console.log('Result', result);
-			// console.log(res.json())
-			// res.send(result.json());
-			res.status(316)
-		})
-		.catch(err => {
-			console.log('Error getting topics:', err)
-			res.status(503);
-		})
-		res.end()
 });
-router.get('/test', function(req, res, next) {
-	res.send('Test')
-});
-
 
 module.exports = router;
