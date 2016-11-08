63762d41ada795563e6533a0a53e8232d574b182
Sean Tracey <seanmtracey@gmail.com>
2016-11-08 15:35:34 +0000

    Get all audio article for passed topics

--- routes/audio/index.js
+++ routes/audio/index.js
@@ -2,101 +2,108 @@ const express = require('express');
 const router = express.Router();
 const capi = require('../../bin/lib/capi');
 const debug = require('debug')('listen-to-the-FT:routes:audio');
-const isUUID = require('is-uuid');
+
 const extractUUID = require('../../bin/lib/extract-uuid');
+const generatePublicS3URL = require('../../bin/lib/get-s3-public-url');
+const validateSession = require('../../bin/lib/validate-session');
 
+router.use(validateSession);
 
-router.get('/', function(req, res, next) {
+function getTopics(req, res){
 
-	const topicIds = req.query.topics.split(',');
-
-	debug(topicIds)
-
-	const articles = topicIds.map(id => {
-		return capi.topic(id);
-	});
-
-	var articleOutput = []
-
-	Promise.all(articles => {
-		console.log(articles);
-		// articleOutput.push(articles);
-		
-	});
-	res.send(articles);
-	
-	// res.end();
-});
-
-router.get('/:topic', function(req, res, next) {
-
-	const topicUUID = req.params.topic;
-	if(isUUID.anyNonNil(topicUUID) === false){
-		debug("Topic wasn't a UUID");
+	if(req.query.topics === undefined){
 		res.status(400);
-		res.send({
-			error : 400,
-			message : "UUID wasn't provided in the URL"
-		})
+		res.json({
+			status : "ERR",
+			reason : 'topics UUIDs must be passed as the query parameter topic',
+			message : 'The request is not valid'
+		});
+		return;
 	}
-	
 
-	return capi.topic(topicUUID)
-	.then(articles => {
-		
-		var goodArticles = articles.map(article => {
-			return extractUUID(article.apiUrl)
-			.then(uuid => {
-				debug(uuid)
-				return capi.content(uuid)	
-			})
-			.then(content => {
-				const annotations = content.annotations;
-				const hasAudio = annotations.some(function(element, index, array){
-					// debug(element.id, process.env.AUDIO_ARTICLES_THING_ID)
-					return element.id == process.env.AUDIO_ARTICLES_THING_ID;
-				});
+	const topicUUIDs = req.query.topics.split(',') ;
 
-				debug(hasAudio)
-				if(hasAudio){
+	extractUUID(process.env.AUDIO_ARTICLES_THING_ID)
+		.then(audioUUID => {
 
-					return Promise.resolve({
-						id : content.id,
-						title : content.title,
-						standfirst : content.standfirst,
-						byline : content.byline,
-						webUrl : content.webUrl,
-						audioUrl : "https://amazons3/bucket/" + content.id.replace('http://www.ft.com/thing/','') + ".mp3"
-					});
-				}
-				else{
-					return Promise.resolve({});
-				}
-			})
-		})
+			return capi.topic(audioUUID)
+				.then(articles => {
+					
+					const goodArticles = articles.map(article => {
+						return extractUUID(article.apiUrl)
+						.then(uuid => {
+							debug(uuid)
+							return capi.content(uuid);
+						})
+						.then(content => {
+							const annotations = content.annotations;
+							content.id = content.id.replace('http://www.ft.com/thing/','');
 
-		Promise.all(goodArticles)
-			.then(data => {
-				data = data.filter(datum => {
-					return datum.id !== undefined;
+							debug(topicUUIDs, annotations);
+
+							const isValidTopic = annotations.some(annotation => {
+								return topicUUIDs.indexOf(annotation.id.replace('http://api.ft.com/things/','')) > -1;
+							});
+
+							debug(isValidTopic);
+							
+							if(isValidTopic){
+
+								return Promise.resolve({
+									id : content.id,
+									title : content.title,
+									standfirst : content.standfirst,
+									byline : content.byline,
+									webUrl : content.webUrl,
+									audioUrl : generatePublicS3URL(content.id)
+								});
+							}
+							else{
+								return Promise.resolve({});
+							}
+						})
+					})
+
+					Promise.all(goodArticles)
+						.then(data => {
+							data = data.filter(datum => {
+								return datum.id !== undefined;
+							})
+
+							if(data.length === 0){
+								res.status(404);
+							}
+							res.json(data);
+
+						})
+						.then(output => res.send(output))
+					;
+
 				})
-				res.json(data);
+				.catch(err => {
+					debug(err);
+					res.status(502);
+					res.send({
+						error : 502,
+						message : `Unable to retrieve content from CAPI ${err.message}`
+					})
+				})
+			;
 
-			})
-			.then(output => res.send(output))
-		;
-
-	})
-	.catch(err => {
-		res.status(502);
-		res.send({
-			error : 502,
-			message : "Unable to retrieve content from CAPI"
 		})
-	})
+		.catch(err => {
+			debug("Topic wasn't a UUID");
+			res.status(400);
+			res.send({
+				error : 400,
+				message : "UUID wasn't provided in the URL"
+			})
+		})
+	;
 
 
+}
 
-});
+router.get('/', getTopics);
 
 module.exports = router;
