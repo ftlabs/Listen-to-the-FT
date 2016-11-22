a0786c3cada76beadb231f804937f4d5621d6bc5
Sean Tracey <seanmtracey@gmail.com>
2016-11-22 13:40:40 +0000

    Formatting...

--- client/service-worker.js
+++ client/service-worker.js
@@ -21,12 +21,13 @@ var itemsToCache = [
 self.addEventListener('install', function(event) {
 
 	event.waitUntil( 
-		caches.open(CACHE_NAME).then(function(cache) {
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
 			console.log('Opened cache');
 			return cache.addAll(itemsToCache);
 
 		})
-
+		
 	);
 
 });
